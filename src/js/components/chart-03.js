9059c7395b59f4268d38ac9772a4090bd264c48e
SAI KIRAN <saikiran4824@gmail.com>
2024-12-20 13:42:30 +0530

    Dashboard and charts integrated

---
+++ src/js/components/chart-03.js
@@ -0,0 +1,55 @@
+import ApexCharts from "apexcharts";
+
+// ===== chartThree
+const chart03 = () => {
+  const chartThreeOptions = {
+    series: [638, 173, 82, 59
+    ],
+    chart: {
+      type: "donut",
+      width: 380,
+    },
+    colors: ["#3C50E0", "#6577F3", "#8FD0EF", "#0FADCF"],
+    labels: ["TESLA", "NISSAN", "KIA", "AUDI"
+],
+    legend: {
+      show: false,
+      position: "bottom",
+    },
+
+    plotOptions: {
+      pie: {
+        donut: {
+          size: "65%",
+          background: "transparent",
+        },
+      },
+    },
+
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
+  };
+
+  const chartSelector = document.querySelectorAll("#chartThree");
+
+  if (chartSelector.length) {
+    const chartThree = new ApexCharts(
+      document.querySelector("#chartThree"),
+      chartThreeOptions
+    );
+    chartThree.render();
+  }
+};
+
+export default chart03;
